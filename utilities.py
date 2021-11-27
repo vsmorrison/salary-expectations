c0622c5138b6365f08e983161925e14537921297
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-11-28 00:42:08 +0300

    added common utilities file for both APIs

--- utilities.py
+++ utilities.py
@@ -1,18 +1,8 @@
-def predict_salaries(raw_salaries):
+def predict_hh_salaries(raw_salaries):
     predicted_salaries = []
     for salary in raw_salaries:
         predicted_salaries.append(predict_hh_rub_salary(salary))
     return predicted_salaries
-
-
-def count_avg_salaries(predicted_salaries):
-    vacancies_processed_values = []
-    avg_salaries = []
-    for salary in predicted_salaries:
-        avg_salary, vacancies_processed = count_avg_salary(salary)
-        avg_salaries.append(avg_salary)
-        vacancies_processed_values.append(vacancies_processed)
-    return avg_salaries, vacancies_processed_values
 
 
 def predict_hh_rub_salary(salaries):
@@ -31,6 +21,41 @@ def predict_hh_rub_salary(salaries):
     return predicted_rub_salaries
 
 
+def predict_sj_rub_salary(salaries):
+    predicted_rub_salaries = []
+    for salary in salaries:
+        if salary is None:
+            predicted_rub_salaries.append(0)
+        elif salary[-1] != 'rub':
+            predicted_rub_salaries.append(0)
+        elif salary[0] == 0 and salary[1] == 0:
+            predicted_rub_salaries.append(0)
+        elif salary[0] == 0 and salary[1] != 0:
+            predicted_rub_salaries.append(salary[1] * 0.8)
+        elif salary[1] == 0 and salary[0] != 0:
+            predicted_rub_salaries.append(salary[0] * 1.2)
+        else:
+            predicted_rub_salaries.append((salary[0] + salary[1]) / 2)
+    return predicted_rub_salaries
+
+
+def predict_sj_salaries(raw_salaries):
+    predicted_salaries = []
+    for salary in raw_salaries:
+        predicted_salaries.append(predict_sj_rub_salary(salary))
+    return predicted_salaries
+
+
+def count_avg_salaries(predicted_salaries):
+    vacancies_processed_values = []
+    avg_salaries = []
+    for salary in predicted_salaries:
+        avg_salary, vacancies_processed = count_avg_salary(salary)
+        avg_salaries.append(avg_salary)
+        vacancies_processed_values.append(vacancies_processed)
+    return avg_salaries, vacancies_processed_values
+
+
 def count_avg_salary(salaries):
     vacancies_processed = 0
     total_salary = 0
@@ -42,14 +67,6 @@ def count_avg_salary(salaries):
     return avg_salary, vacancies_processed
 
 
-# def make_vacancies_stats(vacancies_found, vacancies_processed, avg_salaries):
-#     stats = {}
-#     for vacancy_num, vacancy in enumerate(vacancies_found):
-#         stats[vacancy] = {}
-#         stats[vacancy]['vacancies_found'] = vacancies_found[vacancy]
-#         stats[vacancy]['vacancies_processed'] = vacancies_processed[vacancy_num]
-#         stats[vacancy]['average_salary'] = avg_salaries[vacancy_num]
-#     return stats
 def make_vacancies_stats(vacancies_found, vacancies_processed, avg_salaries):
     stats = []
     stats_by_language = []
