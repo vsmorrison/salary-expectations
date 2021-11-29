0024a4fe8dcb7cb046ec53b20861ffb03b3c019a
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-11-30 02:13:20 +0300

    Fix comparision with None

--- hh_salary_prediction.py
+++ hh_salary_prediction.py
@@ -8,13 +8,13 @@ def predict_hh_salaries(raw_salaries):
 def predict_hh_rub_salary(salaries):
     predicted_rub_salaries = []
     for salary in salaries:
-        if salary is None:
+        if not salary:
             predicted_rub_salaries.append(0)
         elif salary['currency'] != 'RUR':
             predicted_rub_salaries.append(0)
-        elif salary['from'] is None:
+        elif not salary['from']:
             predicted_rub_salaries.append(salary['to'] * 0.8)
-        elif salary['to'] is None:
+        elif not salary['to']:
             predicted_rub_salaries.append(salary['from'] * 1.2)
         else:
             predicted_rub_salaries.append((salary['from'] + salary['to']) / 2)
