35a378f6445c29bdb5aca6580d8c487d7ef9a8eb
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-12-26 01:10:51 +0300

    Delete long comment

--- sj_salary_prediction.py
+++ sj_salary_prediction.py
@@ -1,20 +1,3 @@
-# def predict_sj_rub_salary(salaries):
-#     predicted_rub_salaries = {}
-#     for salary in salaries:
-#         if not salary:
-#             predicted_rub_salaries.append(0)
-#         elif salary[-1] != 'rub':
-#             predicted_rub_salaries.append(0)
-#         elif not salary[0] and not salary[1]:
-#             predicted_rub_salaries.append(0)
-#         elif not salary[0] and salary[1] != 0:
-#             predicted_rub_salaries.append(salary[1] * 0.8)
-#         elif not salary[1] and salary[0] != 0:
-#             predicted_rub_salaries.append(salary[0] * 1.2)
-#         else:
-#             predicted_rub_salaries.append((salary[0] + salary[1]) / 2)
-#     return predicted_rub_salaries
-
 def predict_sj_rub_salary(raw_salaries):
     predicted_rub_salaries = {}
     vacancy = {}
