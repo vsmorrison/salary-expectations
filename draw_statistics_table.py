1b2f6d423ef12063a926ee336ede7fb0a826507b
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-12-27 02:20:16 +0300

    Remake draw function

--- draw_statistics_table.py
+++ draw_statistics_table.py
@@ -1,5 +1,5 @@
 from terminaltables import AsciiTable
-import statistics as stat
+import statistics as s
 
 
 TABLE_DATA = [
@@ -9,35 +9,27 @@ TABLE_DATA = [
     ]
 ]
 
-#
-# def draw_hh_table(table_data):
-#     title = 'HeadHunter Moscow'
-#     output_table = table_data.copy()
-#     for item in stat.make_hh_statistics():
-#         output_table.append(item)
-#     table_instance = AsciiTable(output_table, title)
-#     print(table_instance.table)
-
-
-# def draw_sj_table(table_data):
-#     title = 'SuperJob Moscow'
-#     output_table = table_data.copy()
-#     for item in stat.make_sj_statistics():
-#         output_table.append(item)
-#     table_instance = AsciiTable(output_table, title)
-#     print(table_instance.table)
-
 
 def draw_tables(table_data):
+    row = []
     title_hh = 'HeadHunter Moscow'
     title_sj = 'SuperJob Moscow'
     hh_table = table_data.copy()
     sj_table = table_data.copy()
-    for item in stat.make_hh_statistics():
-        hh_table.append(item)
+    for key, value in s.make_hh_statistics().items():
+        row = [key, value['vacancies_found'], value['vacancies_processed'],
+               value['avg_salary']]
+        hh_table.append(row)
+        row = []
     hh_table_instance = AsciiTable(hh_table, title_hh)
     print(hh_table_instance.table)
-    for item in stat.make_sj_statistics():
-        sj_table.append(item)
+    for key, value in s.make_sj_statistics().items():
+        row = [key, value['vacancies_found'], value['vacancies_processed'],
+               value['avg_salary']]
+        sj_table.append(row)
+        row = []
     sj_table_instance = AsciiTable(sj_table, title_sj)
     print(sj_table_instance.table)
+
+
+draw_tables(TABLE_DATA)
