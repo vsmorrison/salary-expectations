b7044a59bfab29e59e09af346e9c2ba0b11df38d
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-12-27 00:48:06 +0300

    Add dictionary for id

--- headhunter_search.py
+++ headhunter_search.py
@@ -2,17 +2,19 @@ import requests
 
 
 def get_salaries_by_lang(url, languages):
+    input_data = {'Moscow_id': 1, 'SPb_id': 2}
     page = 0
     pages_number = 1
     salaries_by_lang = {}
     vacancies = {}
     vacancy = {}
+    index = 0
     for language in languages:
         salaries_by_lang[language] = {}
         while page < pages_number:
             payload = {
                 'text': f'{language}',
-                'area': '1',
+                'area': f'{input_data["Moscow_id"]}',
                 'period': '30',
                 'page': page
             }
@@ -22,13 +24,14 @@ def get_salaries_by_lang(url, languages):
             pages_number = items['pages']
             print(language, page)
             page += 1
-            for index, item in enumerate(items['items']):
+            for item in items['items']:
                 vacancy['salary'] = item['salary']
                 vacancies[index] = vacancy
+                index += 1
                 vacancy = {}
             salaries_by_lang[language]['vacancies'] = vacancies
-            vacancies = {}
         salaries_by_lang[language]['total'] = items['found']
+        index = 0
+        vacancies = {}
         page = 0
-    print(salaries_by_lang)
     return salaries_by_lang
