65d4cfcf6fda8ed0a25c756361f5a1c790a534d9
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-12-27 02:34:03 +0300

    Edit main function

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import draw_statistics_table as dst
 
 
 def main():
-    dst.draw_hh_table(dst.TABLE_DATA)
-    dst.draw_sj_table(dst.TABLE_DATA)
+    dst.draw_tables(dst.TABLE_DATA)
 
 
 if __name__ == '__main__':
