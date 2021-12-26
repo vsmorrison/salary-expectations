13ca7237e82dcd5aaf0a5dc281774808a38a2d1f
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-12-26 22:26:00 +0300

    Remake response parsing

--- salary_prediction.py
+++ salary_prediction.py
@@ -1,11 +1,27 @@
+def predict_hh_rub_salary(salaries):
+    predicted_rub_salaries = []
+    for salary in salaries:
+        if not salary:
+            predicted_rub_salaries.append(0)
+        elif salary['currency'] != 'RUR':
+            predicted_rub_salaries.append(0)
+        elif not salary['from']:
+            predicted_rub_salaries.append(salary['to'] * 0.8)
+        elif not salary['to']:
+            predicted_rub_salaries.append(salary['from'] * 1.2)
+        else:
+            predicted_rub_salaries.append((salary['from'] + salary['to']) / 2)
+    return predicted_rub_salaries
+
+
+
 def predict_sj_rub_salary(raw_salaries):
     predicted_rub_salaries = {}
     vacancy = {}
     prediction = 0
     for language in raw_salaries:
         predicted_rub_salaries[language] = {}
-        for key, value in raw_salaries[language].items():
-            print(key, value)
+        for key, value in raw_salaries[language]['vacancies'].items():
             if not value:
                 prediction = 0
             elif value['currency'] != 'rub':
@@ -21,10 +37,3 @@ def predict_sj_rub_salary(raw_salaries):
             predicted_rub_salaries[language][key] = prediction
     print(predicted_rub_salaries)
     return predicted_rub_salaries
-
-
-# def predict_sj_salaries(raw_salaries):
-#     predicted_salaries = {}
-#     for salary in raw_salaries:
-#         predicted_salaries.append(predict_sj_rub_salary(salary))
-#     return predicted_salaries
--- superjob_search.py
+++ superjob_search.py
@@ -1,31 +1,14 @@
 import requests
 
 
-def count_vacancies(url, secret_key, languages):
-    num_of_vacancies = {}
-    for language in languages:
-        headers = {
-            'X-Api-App-Id': secret_key
-        }
-        payload = {
-            'keyword': f'программист {language}',
-            'town': 4,
-            'catalogues': 48,
-            'count': 100
-        }
-        response = requests.get(url, headers=headers, params=payload)
-        response.raise_for_status()
-        num_of_vacancies[language] = response.json()['total']
-    return num_of_vacancies
-
-
 def get_salaries_by_lang(url, languages, secret_key):
     page = 0
     pages_number = 1
     salaries_by_lang = {}
     vacancies = {}
+    vacancy = {}
     for language in languages:
-        salaries_by_lang[f'{language}'] = {}
+        salaries_by_lang[language] = {}
         while page < pages_number:
             headers = {
                 'X-Api-App-Id': secret_key
@@ -38,14 +21,18 @@ def get_salaries_by_lang(url, languages, secret_key):
             }
             response = requests.get(url, params=payload, headers=headers)
             response.raise_for_status()
-            items = response.json()['objects']
-            print(language, page)
+            items = response.json()
+            #print(language, page)
             page += 1
-            for index, item in enumerate(items):
-                vacancies['from'] = item['payment_from']
-                vacancies['to'] = item['payment_to']
-                vacancies['currency'] = item['currency']
-                salaries_by_lang[f'{language}'][index] = vacancies
-                vacancies = {}
+            for index, item in enumerate(items['objects']):
+                vacancy['from'] = item['payment_from']
+                vacancy['to'] = item['payment_to']
+                vacancy['currency'] = item['currency']
+                vacancies[index] = vacancy
+                vacancy = {}
+            salaries_by_lang[language]['vacancies'] = vacancies
+            vacancies = {}
+        salaries_by_lang[language]['total'] = items['total']
         page = 0
+    print(salaries_by_lang)
     return salaries_by_lang
