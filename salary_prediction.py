39002618326f936411b08fe3453d47678cdff396
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-12-27 00:57:12 +0300

    Remove comments

--- salary_prediction.py
+++ salary_prediction.py
@@ -1,16 +1,21 @@
-def predict_hh_rub_salary(salaries):
-    predicted_rub_salaries = []
-    for salary in salaries:
-        if not salary:
-            predicted_rub_salaries.append(0)
-        elif salary['currency'] != 'RUR':
-            predicted_rub_salaries.append(0)
-        elif not salary['from']:
-            predicted_rub_salaries.append(salary['to'] * 0.8)
-        elif not salary['to']:
-            predicted_rub_salaries.append(salary['from'] * 1.2)
-        else:
-            predicted_rub_salaries.append((salary['from'] + salary['to']) / 2)
+def predict_hh_rub_salary(raw_salaries):
+    predicted_rub_salaries = {}
+    vacancy = {}
+    prediction = 0
+    for language in raw_salaries:
+        predicted_rub_salaries[language] = {}
+        for key, value in raw_salaries[language]['vacancies'].items():
+            if not value['salary']:
+                prediction = 0
+            elif value['salary']['currency'] != 'RUR':
+                prediction = 0
+            elif not value['salary']['from']:
+                prediction = value['salary']['to'] * 0.8
+            elif not value['salary']['to']:
+                prediction = value['salary']['from'] * 1.2
+            else:
+                prediction = (value['salary']['from'] + value['salary']['to']) / 2
+            predicted_rub_salaries[language][key] = prediction
     return predicted_rub_salaries
 
 
@@ -35,5 +40,4 @@ def predict_sj_rub_salary(raw_salaries):
             else:
                 prediction = (value['to'] + value['from']) / 2
             predicted_rub_salaries[language][key] = prediction
-    print(predicted_rub_salaries)
     return predicted_rub_salaries
