84069177b662bfacce8950115453fe4335972d8b
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-12-27 23:14:21 +0300

    Fix for empty values

--- salary_count.py
+++ salary_count.py
@@ -8,9 +8,13 @@ def count_avg_salaries(predicted_salaries):
             if value:
                 total_salary += value
                 vacancies_processed += 1
-        avg_salary = int(total_salary / vacancies_processed)
-        avg_salaries[language]['avg_salary'] = avg_salary
-        avg_salaries[language]['vacancies_processed'] = vacancies_processed
+        if not vacancies_processed:
+            avg_salaries[language]['avg_salary'] = total_salary
+            avg_salaries[language]['vacancies_processed'] = vacancies_processed
+        else:
+            avg_salary = int(total_salary / vacancies_processed)
+            avg_salaries[language]['avg_salary'] = avg_salary
+            avg_salaries[language]['vacancies_processed'] = vacancies_processed
         total_salary = 0
         vacancies_processed = 0
     return avg_salaries
