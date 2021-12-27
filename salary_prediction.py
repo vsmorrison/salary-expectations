f711ee3750676dd56ee311b73f63fd98e965cd59
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-12-27 21:52:59 +0300

    Add fix for empty dict

--- salary_prediction.py
+++ salary_prediction.py
@@ -25,18 +25,21 @@ def predict_sj_rub_salary(raw_salaries):
     prediction = 0
     for language in raw_salaries:
         predicted_rub_salaries[language] = {}
-        for key, value in raw_salaries[language]['vacancies'].items():
-            if not value:
-                prediction = 0
-            elif value['currency'] != 'rub':
-                prediction = 0
-            elif not value['from'] and not value['to']:
-                prediction = 0
-            elif not value['from'] and value['to'] != 0:
-                prediction = value['to'] * 0.8
-            elif not value['to'] and value['from'] != 0:
-                prediction = value['from'] * 1.2
-            else:
-                prediction = (value['to'] + value['from']) / 2
-            predicted_rub_salaries[language][key] = prediction
+        for key, value in raw_salaries[language].items():
+            if not raw_salaries[language]['total']:
+                predicted_rub_salaries[language] = {0: 0}
+            for key, value in raw_salaries[language]['vacancies'].items():
+                if not value:
+                    prediction = 0
+                elif value['currency'] != 'rub':
+                    prediction = 0
+                elif not value['from'] and not value['to']:
+                    prediction = 0
+                elif not value['from'] and value['to'] != 0:
+                    prediction = value['to'] * 0.8
+                elif not value['to'] and value['from'] != 0:
+                    prediction = value['from'] * 1.2
+                else:
+                    prediction = (value['to'] + value['from']) / 2
+                predicted_rub_salaries[language][key] = prediction
     return predicted_rub_salaries
