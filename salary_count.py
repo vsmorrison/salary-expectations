fe46e37b9e3c2cb7f6f14cb587ea8fd45f42c4ef
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-12-30 00:01:59 +0300

    Add new logic

--- salary_count.py
+++ salary_count.py
@@ -1,20 +1,36 @@
 def count_avg_salaries(predicted_salaries):
-    avg_salaries = {}
+    avg_salary = 0
     total_salary = 0
     vacancies_processed = 0
-    for language in predicted_salaries:
-        avg_salaries[language] = {}
-        for value in predicted_salaries[language].values():
-            if value:
-                total_salary += value
-                vacancies_processed += 1
-        if not vacancies_processed:
-            avg_salaries[language]['avg_salary'] = total_salary
-            avg_salaries[language]['vacancies_processed'] = vacancies_processed
-        else:
-            avg_salary = int(total_salary / vacancies_processed)
-            avg_salaries[language]['avg_salary'] = avg_salary
-            avg_salaries[language]['vacancies_processed'] = vacancies_processed
-        total_salary = 0
-        vacancies_processed = 0
-    return avg_salaries
+    for salary in predicted_salaries:
+        if salary:
+            total_salary += salary
+            vacancies_processed += 1
+    if not vacancies_processed:
+        avg_salary = total_salary
+        vacancies_processed = vacancies_processed
+    else:
+        avg_salary = int(total_salary / vacancies_processed)
+    return avg_salary, vacancies_processed
+
+
+# def count_avg_salaries(predicted_salaries):
+#     avg_salaries = []
+#     total_salary = 0
+#     vacancies_processed = 0
+#     for language in predicted_salaries:
+#         avg_salaries[language] = {}
+#         for value in predicted_salaries[language].values():
+#             if value:
+#                 total_salary += value
+#                 vacancies_processed += 1
+#         if not vacancies_processed:
+#             avg_salaries[language]['avg_salary'] = total_salary
+#             avg_salaries[language]['vacancies_processed'] = vacancies_processed
+#         else:
+#             avg_salary = int(total_salary / vacancies_processed)
+#             avg_salaries[language]['avg_salary'] = avg_salary
+#             avg_salaries[language]['vacancies_processed'] = vacancies_processed
+#         total_salary = 0
+#         vacancies_processed = 0
+#     return avg_salaries
