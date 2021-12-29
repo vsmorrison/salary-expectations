6badd19dd1fa5917cd0d0cd140b33a015d62c225
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-12-30 00:02:53 +0300

    Add filter function and remake predict function

--- salary_prediction.py
+++ salary_prediction.py
@@ -1,3 +1,39 @@
+def filter_sj_vacancies(raw_salaries):
+    filtered_sj_vacancies = []
+    salary = []
+    for vacancy in raw_salaries:
+        salary.append(vacancy['payment_from'])
+        salary.append(vacancy['payment_to'])
+        salary.append(vacancy['currency'])
+        if not salary[0] and not salary[1] or salary[2] != 'rub':
+            salary = 0
+            filtered_sj_vacancies.append(salary)
+        else:
+            filtered_sj_vacancies.append(salary[0:2])
+        salary = []
+    return filtered_sj_vacancies
+
+
+def predict_rub_salaries(filtered_vacancies):
+    predicted_salaries = []
+    prediction = 0
+    for vacancy in filtered_vacancies:
+        if vacancy:
+            if vacancy[0] == 0:
+                prediction = vacancy[1] * 0.8
+            elif not vacancy[1]:
+                prediction = vacancy[0] * 1.2
+            else:
+                prediction = (vacancy[0] + vacancy[1]) / 2
+            predicted_salaries.append(prediction)
+        else:
+            predicted_salaries.append(vacancy)
+    return predicted_salaries
+
+
+
+
+
 def predict_hh_rub_salary(raw_salaries):
     predicted_rub_salaries = {}
     vacancy = {}
