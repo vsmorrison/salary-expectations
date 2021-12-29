ba3bd226f6aea2cd86b225864ca73ec21ca9effd
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-12-30 00:01:15 +0300

    Function returns whole vacations list

--- superjob_search.py
+++ superjob_search.py
@@ -1,37 +1,25 @@
 import requests
 
 
-def get_salaries_by_lang(url, languages, secret_key):
+def get_salaries_by_lang(url, language, secret_key):
     input_data = {'Moscow_id': 4, 'SPb_id': 14, 'SW_Development_id': 48}
     page = 0
     pages_number = 1
-    salaries_by_lang = {}
-    vacancies = {}
-    vacancy = {}
-    for language in languages:
-        salaries_by_lang[language] = {}
-        while page < pages_number:
-            headers = {
-                'X-Api-App-Id': secret_key
-            }
-            payload = {
-                'keyword': f'программист {language}',
-                'town': f'{input_data["SPb_id"]}',
-                'catalogues': f'{input_data["SW_Development_id"]}',
-                'count': 100
-            }
-            response = requests.get(url, params=payload, headers=headers)
-            response.raise_for_status()
-            items = response.json()
-            page += 1
-            for index, item in enumerate(items['objects']):
-                vacancy['from'] = item['payment_from']
-                vacancy['to'] = item['payment_to']
-                vacancy['currency'] = item['currency']
-                vacancies[index] = vacancy
-                vacancy = {}
-            salaries_by_lang[language]['vacancies'] = vacancies
-            vacancies = {}
-        salaries_by_lang[language]['total'] = items['total']
-        page = 0
-    return salaries_by_lang
+    salaries_by_lang = []
+    while page < pages_number:
+        headers = {
+            'X-Api-App-Id': secret_key
+        }
+        payload = {
+            'keyword': f'программист {language}',
+            'town': f'{input_data["SPb_id"]}',
+            'catalogues': f'{input_data["SW_Development_id"]}',
+            'count': 100
+        }
+        response = requests.get(url, params=payload, headers=headers)
+        response.raise_for_status()
+        items = response.json()
+        page += 1
+    salaries_by_lang = items['objects']
+    total = items['total']
+    return salaries_by_lang, total
