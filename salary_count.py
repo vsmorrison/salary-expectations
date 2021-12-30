8b25a98b67f2eebedf4c8f3cf56393e537d1547f
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-12-30 13:10:42 +0300

    Delete odd comment

--- salary_count.py
+++ salary_count.py
@@ -12,25 +12,3 @@ def count_avg_salaries(predicted_salaries):
     else:
         avg_salary = int(total_salary / vacancies_processed)
     return avg_salary, vacancies_processed
-
-
-# def count_avg_salaries(predicted_salaries):
-#     avg_salaries = []
-#     total_salary = 0
-#     vacancies_processed = 0
-#     for language in predicted_salaries:
-#         avg_salaries[language] = {}
-#         for value in predicted_salaries[language].values():
-#             if value:
-#                 total_salary += value
-#                 vacancies_processed += 1
-#         if not vacancies_processed:
-#             avg_salaries[language]['avg_salary'] = total_salary
-#             avg_salaries[language]['vacancies_processed'] = vacancies_processed
-#         else:
-#             avg_salary = int(total_salary / vacancies_processed)
-#             avg_salaries[language]['avg_salary'] = avg_salary
-#             avg_salaries[language]['vacancies_processed'] = vacancies_processed
-#         total_salary = 0
-#         vacancies_processed = 0
-#     return avg_salaries
