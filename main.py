dc96bbcdd3f51fe5be895be6cdb99a4414ca934f
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-12-30 13:17:09 +0300

    Edit main logic

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import os
 from dotenv import load_dotenv
+import statistics as s
 import draw_statistics_table as dst
 
 
 def main():
     load_dotenv()
     sj_secret_key = os.getenv('SJ_SECRET_KEY')
-    dst.draw_tables(dst.TABLE_DATA)
+    languages = [
+        'Javascript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'Go'
+    ]
+    hh_stat = s.make_hh_statistics(languages)
+    hh_table = dst.draw_tables(hh_stat, 'HeadHunter Moscow')
+    sj_stat = s.make_sj_statistics(languages, sj_secret_key)
+    sj_table = dst.draw_tables(sj_stat, 'SuperJob Moscow')
+    print(sj_table.table)
+    print(hh_table.table)
 
 
 if __name__ == '__main__':
