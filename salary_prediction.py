ec2570c7d55aa80f8be0d17742499b7a6da2d35b
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2021-12-30 13:13:14 +0300

    Add hh parsing logic

--- salary_prediction.py
+++ salary_prediction.py
@@ -14,12 +14,37 @@ def filter_sj_vacancies(raw_salaries):
     return filtered_sj_vacancies
 
 
+def filter_hh_vacancies(raw_salaries):
+    filtered_hh_vacancies = []
+    salary = []
+    filtered_salary = []
+    for page in raw_salaries:
+        for vacancy in page:
+            salary.append(vacancy['salary'])
+    for item in salary:
+        if not item:
+            item = 0
+            filtered_hh_vacancies.append(item)
+        elif item['currency'] != 'RUR':
+            item = 0
+            filtered_hh_vacancies.append(item)
+        else:
+            if not item['from']:
+                filtered_salary = [0, item['to']]
+            elif not item['to']:
+                filtered_salary = [item['from'], 0]
+            else:
+                filtered_salary = [item['from'], item['to']]
+            filtered_hh_vacancies.append(filtered_salary)
+    return filtered_hh_vacancies
+
+
 def predict_rub_salaries(filtered_vacancies):
     predicted_salaries = []
     prediction = 0
     for vacancy in filtered_vacancies:
         if vacancy:
-            if vacancy[0] == 0:
+            if not vacancy[0]:
                 prediction = vacancy[1] * 0.8
             elif not vacancy[1]:
                 prediction = vacancy[0] * 1.2
@@ -29,50 +54,3 @@ def predict_rub_salaries(filtered_vacancies):
         else:
             predicted_salaries.append(vacancy)
     return predicted_salaries
-
-
-
-
-
-def predict_hh_rub_salary(raw_salaries):
-    predicted_rub_salaries = {}
-    vacancy = {}
-    prediction = 0
-    for language in raw_salaries:
-        predicted_rub_salaries[language] = {}
-        for key, value in raw_salaries[language]['vacancies'].items():
-            if not value['salary']:
-                prediction = 0
-            elif value['salary']['currency'] != 'RUR':
-                prediction = 0
-            elif not value['salary']['from']:
-                prediction = value['salary']['to'] * 0.8
-            elif not value['salary']['to']:
-                prediction = value['salary']['from'] * 1.2
-            else:
-                prediction = (value['salary']['from'] + value['salary']['to']) / 2
-            predicted_rub_salaries[language][key] = prediction
-    return predicted_rub_salaries
-
-
-def predict_sj_rub_salary(raw_salaries):
-    predicted_rub_salaries = {}
-    vacancy = {}
-    prediction = 0
-    for language in raw_salaries:
-        predicted_rub_salaries[language] = {}
-        for key, value in raw_salaries[language]['vacancies'].items():
-            if not value:
-                prediction = 0
-            elif value['currency'] != 'rub':
-                prediction = 0
-            elif not value['from'] and not value['to']:
-                prediction = 0
-            elif not value['from'] and value['to'] != 0:
-                prediction = value['to'] * 0.8
-            elif not value['to'] and value['from'] != 0:
-                prediction = value['from'] * 1.2
-            else:
-                prediction = (value['to'] + value['from']) / 2
-            predicted_rub_salaries[language][key] = prediction
-    return predicted_rub_salaries
