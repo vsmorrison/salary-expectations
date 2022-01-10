8459cb8e8b85eb0fd370556f691dfdcc17f5995c
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-01-11 00:36:15 +0300

    Moved api links into main

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ import draw_statistics_table
 def main():
     load_dotenv()
     sj_secret_key = os.getenv('SJ_SECRET_KEY')
+    api_urls = {
+        'SuperJob': 'https://api.superjob.ru/2.0/vacancies/',
+        'HeadHunter': 'https://api.hh.ru/vacancies'
+    }
     languages = [
-        'Javascript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'Go'
+        'Ruby', 'Go'
     ]
-    hh_stat = headhunter_search.make_hh_statistics(languages)
+    # languages = [
+    #     'Javascript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'Go'
+    # ]
+    hh_stat = headhunter_search.make_hh_statistics(languages, api_urls['HeadHunter'])
     hh_table = draw_statistics_table.draw_tables(hh_stat, 'HeadHunter Moscow')
-    sj_stat = superjob_search.make_sj_statistics(languages, sj_secret_key)
+    sj_stat = superjob_search.make_sj_statistics(languages, sj_secret_key, api_urls['SuperJob'])
     sj_table = draw_statistics_table.draw_tables(sj_stat, 'SuperJob Moscow')
     print(hh_table.table)
     print(sj_table.table)
