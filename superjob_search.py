8670b3f5065d3a3c50860ac87b42f7ed0c4478de
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-01-11 00:17:40 +0300

    Add statistics function

--- superjob_search.py
+++ superjob_search.py
@@ -1,11 +1,12 @@
 import requests
+import salary_count as sc
+import salary_prediction as sp
 
 
 def get_salaries_by_lang(url, language, secret_key):
     input_data = {'Moscow_id': 4, 'SPb_id': 14, 'SW_Development_id': 48}
     page = 0
     pages_number = 1
-    salaries_by_lang = []
     while page < pages_number:
         headers = {
             'X-Api-App-Id': secret_key
@@ -23,3 +24,18 @@ def get_salaries_by_lang(url, language, secret_key):
     salaries_by_lang = items['objects']
     total = items['total']
     return salaries_by_lang, total
+
+
+def make_sj_statistics(languages, secret_key):
+    statistics = {}
+    sj_url = 'https://api.superjob.ru/2.0/vacancies/'
+    for language in languages:
+        raw_salaries, total = get_salaries_by_lang(sj_url, language, secret_key)
+        filtered_sj_vacancies = sp.filter_sj_vacancies(raw_salaries)
+        predicted_salaries = sp.predict_rub_salaries(filtered_sj_vacancies)
+        avg_salary, vacancies_processed = sc.count_avg_salaries(predicted_salaries)
+        statistics[language] = {}
+        statistics[language]['total'] = total
+        statistics[language]['vacancies_processed'] = vacancies_processed
+        statistics[language]['avg_salary'] = avg_salary
+    return statistics
