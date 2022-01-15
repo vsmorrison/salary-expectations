77499d4f38763b79de292baec679c74a7c6f5148
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-01-15 14:54:31 +0300

    Change count salary logic

--- superjob_search.py
+++ superjob_search.py
@@ -3,58 +3,48 @@ import salary_prediction
 from itertools import count
 
 
-def get_salaries_by_lang(url, language, secret_key, town, catalogues):
-    salaries_by_lang = []
+def get_vacancies_by_lang(language, secret_key, town, catalogues):
+    url = 'https://api.superjob.ru/2.0/vacancies'
+    vacancies_by_lang = []
+    headers = {
+        'X-Api-App-Id': secret_key
+    }
+    payload = {
+        'keyword': f'программист {language}',
+        'town': town,
+        'catalogues': catalogues,
+        'count': 100
+    }
     for page in count():
-        headers = {
-            'X-Api-App-Id': secret_key
-        }
-        payload = {
-            'keyword': f'программист {language}',
-            'town': town,
-            'catalogues': catalogues,
-            'page': page,
-            'count': 100
-        }
+        payload['page'] = page
         response = requests.get(url, params=payload, headers=headers)
         response.raise_for_status()
-        items = response.json()
-        salaries_by_lang.extend(items['objects'])
-        if not items['more']:
+        vacancies = response.json()
+        vacancies_by_lang.extend(vacancies['objects'])
+        if not vacancies['more']:
             break
-    total = items['total']
-    return salaries_by_lang, total
+    total = vacancies['total']
+    return vacancies_by_lang, total
 
 
-def count_sj_salaries(raw_salaries):
-    total_salary = 0
-    avg_salary = 0
-    vacancies_processed = 0
-    for vacancy in raw_salaries:
-        if vacancy['currency'] == 'rub':
-            salary_from = vacancy['payment_from']
-            salary_to = vacancy['payment_to']
-        else:
-            salary_from = 0
-            salary_to = 0
-        predicted_salary = salary_prediction.predict_rub_salaries(
-            salary_from, salary_to
-        )
-        if predicted_salary:
-            total_salary += predicted_salary
-            vacancies_processed += 1
-    if vacancies_processed:
-        avg_salary = int(total_salary / vacancies_processed)
-    return avg_salary, vacancies_processed
+def count_sj_predicted_salary(vacancy):
+    if vacancy['currency'] != 'rub':
+        return None
+    return salary_prediction.predict_rub_salary(
+        vacancy['payment_from'], vacancy['payment_to']
+    )
 
 
-def make_sj_statistics(languages, secret_key, url, town, catalogues):
+def make_sj_statistics(languages, secret_key, town, catalogues):
     statistics = {}
     for language in languages:
-        raw_salaries, total = get_salaries_by_lang(
-            url, language, secret_key, town, catalogues
+        raw_vacancies, total = get_vacancies_by_lang(
+            language, secret_key, town, catalogues
         )
-        avg_salary, vacancies_processed = count_sj_salaries(raw_salaries)
+        avg_salary, vacancies_processed = \
+            salary_prediction.count_avg_salaries(
+                raw_vacancies, count_sj_predicted_salary
+            )
         statistics[language] = {
             'total': total,
             'vacancies_processed': vacancies_processed,
